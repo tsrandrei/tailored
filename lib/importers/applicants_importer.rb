58960a88fb250f0f4f9329b888dc5f76faef5582
tsrandrei <claudiu.andrei.roman@gmail.com>
2019-02-06 23:05:51 +0100

    Rescue if in CSV - email field encounter phone as in data provided.

--- lib/importers/applicants_importer.rb
+++ lib/importers/applicants_importer.rb
@@ -4,30 +4,31 @@ require 'csv'
 
 module Importers
   class ApplicantsImporter
-    class InvalidApplicantEmailError < StandardError; end
+    class EmailPhoneError < StandardError
+    end
 
     def import
       CSV.foreach(ARGV[1].to_s, headers: true) do |line|
-        raise InvalidApplicantEmailError unless line['E-mail'].match(URI::MailTo::EMAIL_REGEXP)
+        raise EmailPhoneError unless line['E-mail'].match(URI::MailTo::EMAIL_REGEXP)
 
         JobApplication.create!(
           candidate_id: find_or_create_candidate(line).id,
           job_id: find_or_create_job(line).id
         )
-      rescue InvalidApplicantEmailError
+      rescue EmailPhoneError
         p 'Invalid record to be written in DB : ' + line.to_s
+        candidate_with_phone_as_email(line)
       end
     end
 
-    private
+      private
 
     def find_or_create_candidate(line)
       Candidate.where(email: line['E-mail']).first || create_candidate(line)
     end
 
     def create_candidate(line)
-      raise InvalidApplicantEmailError unless
-          line['E-mail'].match(URI::MailTo::EMAIL_REGEXP)
+      raise EmailPhoneError unless line['E-mail'].match(URI::MailTo::EMAIL_REGEXP)
 
       Candidate.create!(
         full_name: line['Name'],
@@ -36,17 +37,27 @@ module Importers
         note: nil,
         created_at: line['Created at']
       )
+      end
     end
 
-    def find_or_create_job(line)
-      Job.where(candidate_id: line['E-mail']).first || create_job(line)
-    end
-
-    def create_job(line)
-      Job.create!(
-        title: line['Job'],
-        candidate_id: Candidate.where(email: line['E-mail']).first.id
-      )
-    end
+  def candidate_with_phone_as_email(line)
+    Candidate.create!(
+      full_name: line['Name'],
+      email: nil,
+      phone: line['E-mail'],
+      note: nil,
+      created_at: line['Created at']
+    )
   end
-end
+
+  def find_or_create_job(line)
+    Job.where(candidate_id: line['E-mail']).first || create_job(line)
+  end
+
+  def create_job(line)
+    Job.create!(
+      title: line['Job'],
+      candidate_id: Candidate.where(email: line['E-mail']).first.id
+    )
+  end
+  end
