e4912f7d5b0692c23fb79237f9062f58c01c65fe
tsrandrei <claudiu.andrei.roman@gmail.com>
2019-02-07 14:41:13 +0100

    Updated Applicants Importer

--- lib/importers/applicants_importer.rb
+++ lib/importers/applicants_importer.rb
@@ -9,15 +9,12 @@ module Importers
 
     def import
       CSV.foreach(ARGV[1].to_s, headers: true) do |line|
-        raise EmailPhoneError unless line['E-mail'].match(URI::MailTo::EMAIL_REGEXP)
 
         JobApplication.create!(
           candidate_id: find_or_create_candidate(line).id,
-          job_id: find_or_create_job(line).id
+          job_id: find_or_create_job(line).id,
+          submission_date: line['Created at']
         )
-      rescue EmailPhoneError
-        p 'Invalid record to be written in DB : ' + line.to_s
-        candidate_with_phone_as_email(line)
       end
     end
 
@@ -37,6 +34,9 @@ module Importers
         note: nil,
         created_at: line['Created at']
       )
+    rescue EmailPhoneError
+      p 'Invalid Candidate record to be written in DB : ' + line.to_s
+      candidate_with_phone_as_email(line)
     end
 
     def candidate_with_phone_as_email(line)
@@ -54,9 +54,20 @@ module Importers
     end
 
     def create_job(line)
+      raise EmailPhoneError unless line['E-mail'].match(URI::MailTo::EMAIL_REGEXP)
       Job.create!(
         title: line['Job'],
         candidate_id: Candidate.where(email: line['E-mail']).first.id
+      )
+    rescue EmailPhoneError
+      # p 'Invalid JOB record to be written in DB : ' + line.to_s
+      job_with_email_nil(line)
+    end
+
+    def job_with_email_nil(line)
+      Job.create!(
+          title: line['Job'],
+          candidate_id: Candidate.where(phone: line['Phone']).first.id
       )
     end
   end
